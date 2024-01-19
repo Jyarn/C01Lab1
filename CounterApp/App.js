4c55a7a88326b896287d8dbafad69169e8805bbe
flight <ToothPaste32@outlook.com>
2024-01-19 12:49:22 -0500

    update package.json

---
+++ CounterApp/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import Counter from './components/Counter';
+
+Counter.defaultProps = {
+    initialValues: [0],
+};
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+        <Counter initialValues={[5, 10, 15]} />
+        <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
